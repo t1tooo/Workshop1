3ca75c90fd54cbd233f77e83bc632d14c05e2b31
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-15 19:46:02 +0200

    update again

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -6,6 +6,11 @@ import chrome from 'selenium-webdriver/chrome.js';
 const options = new chrome.Options();
 headless && options.addArguments('--headless=new');
 
+// Get the user-data-dir from the environment variable (set by the GitHub Actions workflow)
+const userDataDir = process.env.USER_DATA_DIR || './default_user_data';
+
+options.addArguments(`--user-data-dir=${userDataDir}`);
+
 class CustomWorld {
   constructor() {
     this.driver = new seleniumWebdriver
