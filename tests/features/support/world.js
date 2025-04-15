90d10e9c812ae03a7629cac44e413649e5dd69da
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-15 19:41:48 +0200

    update

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -3,16 +3,8 @@ import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
 import { timeout, browser, headless } from '../../config.js';
 import chrome from 'selenium-webdriver/chrome.js';
 
-// Set up Chrome options to remove any previous user-data-dir
 const options = new chrome.Options();
-
-// Ensure no user-data-dir is being used
-options.addArguments('--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu');
-
-// Optional: headless mode based on the configuration
-if (headless) {
-  options.addArguments('--headless');
-}
+headless && options.addArguments('--headless=new');
 
 class CustomWorld {
   constructor() {
