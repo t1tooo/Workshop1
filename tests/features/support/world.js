b0ab1e97e358403323c671d9973f8eb2b130a52d
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-18 20:34:35 +0200

    0o

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -3,6 +3,7 @@ import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
 import { timeout, browser, headless } from '../../config.js';
 import chrome from 'selenium-webdriver/chrome.js';
 
+
 const options = new chrome.Options();
 headless && options.addArguments('--headless=new');
 
