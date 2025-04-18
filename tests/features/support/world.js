54117f423feb50f9088f66a6d395134fb121dc99
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-18 20:48:35 +0200

    Update world.js

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -2,9 +2,17 @@ import seleniumWebdriver from 'selenium-webdriver';
 import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
 import { timeout, browser, headless } from '../../config.js';
 import chrome from 'selenium-webdriver/chrome.js';
+import path from 'path';
+import os from 'os';
 
 const options = new chrome.Options();
-if (headless) options.addArguments('--headless=new');
+
+if (headless) options.addArguments('--headless'); // headless mode
+
+// Use a unique directory for user data to avoid conflicts
+const userDataDir = path.join(os.tmpdir(), `chrome_user_data_${Date.now()}`);
+
+options.addArguments(`--user-data-dir=${userDataDir}`);
 
 class CustomWorld {
   constructor() {
