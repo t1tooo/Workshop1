12bc1f7b2a2a88db0f29a6ba6f87d66b81df68c6
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-18 20:22:51 +0200

    Update world.js

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,35 +1,20 @@
-import { Builder } from 'selenium-webdriver';
+import seleniumWebdriver from 'selenium-webdriver';
 import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
-import chrome from 'selenium-webdriver/chrome.js';
 import { timeout, browser, headless } from '../../config.js';
+import chrome from 'selenium-webdriver/chrome.js';
 
-// Set the default timeout for Cucumber
-setDefaultTimeout(timeout);
-
-// Configure Chrome options
 const options = new chrome.Options();
+headless && options.addArguments('--headless=new');
 
-// Optionally, enable headless mode if needed
-if (headless) {
-  options.addArguments('--headless=new');
-}
-
-// Add other Chrome options that help with stability
-options.addArguments('--disable-dev-shm-usage');
-options.addArguments('--no-sandbox');
-options.addArguments('--disable-gpu'); // Disable GPU hardware acceleration in headless mode
-options.addArguments('--incognito');  // Use incognito mode to avoid saving session data
-
-// Custom World constructor for each test scenario
 class CustomWorld {
   constructor() {
-    // Initialize the WebDriver for each test
-    this.driver = new Builder()
+    this.driver = new seleniumWebdriver
+      .Builder()
       .setChromeOptions(options)
       .forBrowser(browser)
       .build();
   }
 }
 
-// Set the world constructor in Cucumber
+setDefaultTimeout(timeout);
 setWorldConstructor(CustomWorld);
