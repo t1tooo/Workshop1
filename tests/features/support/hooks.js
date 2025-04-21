e7791e580c7fa21b223ba0113fb73c505dec94a2
t1tooo <146703783+t1tooo@users.noreply.github.com>
2025-04-21 20:15:08 +0200

    UPDATE

--- tests/features/support/hooks.js
+++ tests/features/support/hooks.js
@@ -1,30 +1,18 @@
-import { Before, After, AfterStep } from '@cucumber/cucumber';
+import { BeforeAll, AfterAll, AfterStep } from '@cucumber/cucumber';
 import { sleepBetweenSteps } from '../../config.js';
-import { Builder } from 'selenium-webdriver';
-import chrome from 'selenium-webdriver/chrome.js';
+import { driver } from './world.js';
 
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-Before(async function () {
-  if (!this.driver) {
-    const options = new chrome.Options();
-    options.addArguments('--headless');
-
-    this.driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build();
-  }
-  await this.driver.manage().window().maximize();
+BeforeAll(function () {
+  return driver.manage().window().maximize();
 });
 
-After(async function () {
-  if (this.driver) {
-    await this.driver.quit();
-    this.driver = null;
-  }
+AfterAll(function () {
+  return driver.quit();
 });
 
 AfterStep(async function () {
   await sleep(sleepBetweenSteps);
 });
+
--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -1,47 +1,20 @@
-// Import necessary modules
-import { setWorldConstructor } from '@cucumber/cucumber';
-import { Builder } from 'selenium-webdriver';
+import seleniumWebdriver from 'selenium-webdriver';
+import { setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
+import { timeout, browser, headless } from '../../config.js';
 import chrome from 'selenium-webdriver/chrome.js';
-import path from 'path';
-import fs from 'fs';
 
-// Set up the custom World constructor
+const options = new chrome.Options();
+headless && options.addArguments('--headless=new');
+
 class CustomWorld {
   constructor() {
-    this.driver = null;
-  }
-
-  async initDriver() {
-    // Generate a unique temporary directory for user data
-    const tempDir = path.join(new URL('.', import.meta.url).pathname, `tmp/chrome-user-data-${Math.random().toString(36).substring(2, 15)}`);
-
-    // Create the directory if it doesn't exist
-    fs.mkdirSync(tempDir, { recursive: true });
-
-    // Set up Chrome options
-    const options = new chrome.Options();
-    options.addArguments(`--user-data-dir=${tempDir}`);  // Specify the unique user data directory
-
-    try {
-      // Create a new WebDriver instance with the specified options
-      this.driver = await new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(options)
-        .build();
-
-      // Optionally navigate to a page
-      await this.driver.get('http://localhost:3000');
-    } catch (error) {
-      console.error('Error during WebDriver session creation:', error);
-    }
-  }
-
-  async closeDriver() {
-    if (this.driver) {
-      await this.driver.quit();
-    }
+    this.driver = new seleniumWebdriver
+      .Builder()
+      .setChromeOptions(options)
+      .forBrowser(browser)
+      .build();
   }
 }
 
-// Set the custom World constructor
+setDefaultTimeout(timeout);
 setWorldConstructor(CustomWorld);
